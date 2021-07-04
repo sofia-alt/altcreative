1795a5ed510f331906e0fd564f23e2bfcbdc32bd
Sofia <sofia@MacBook-Air-Sofia.local>
2021-07-05 01:18:56 +0300

    fonts and styles navigation

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -59,12 +59,5 @@ setup () {
 </script>
 
 <style>
-#app {
-font-family: Avenir, Helvetica, Arial, sans-serif;
--webkit-font-smoothing: antialiased;
--moz-osx-font-smoothing: grayscale;
-text-align: center;
-color: #2c3e50;
-margin-top: 60px;
-}
+
 </style>
